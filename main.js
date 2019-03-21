5057455fa6351d5ef4987ca019a8a864fd45fb6b
Jin Kwon <krnjin94@gmail.com>
2019-03-20 18:23:06 -0700

    working on createGrid

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 const container = document.querySelector("#container");
 
+function createGrid(x){
+    for (var row = 0; row < x; row++){
+        for (var col = 0; col < x; col++){
+            $("#container").append("<div id='grid'></div>");
+        };
+    };
+
+    $("#grid").height(1000/x);
+    $("#grid").width(1000/x)
+
+}
+
+
+$(document).ready(function()){
+    createGrid(16)
+}
